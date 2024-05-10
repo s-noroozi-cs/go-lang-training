577f926231a8946f9b3b5572011c626e573d47d0
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-10 15:52:04 +0330

    Update hello.go

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -8,4 +8,9 @@ func main() {
 	msg = `Hello 
 "world"`
 	fmt.Println(msg)
+	var x int = 10
+	var y float64 = 30.2
+	var sumf float64 = float64(x) + y
+	var sumi int = x + int(y)
+	fmt.Println(sumf, sumi)
 }

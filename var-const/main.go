b8e8740458ee6490cb96fa568a81f2c07526cb46
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-10 17:11:00 +0330

    refactor

--- var-const/main.go
+++ var-const/main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ok")
+	var i int = 20
+	var f float64 = float64(i)
+	fmt.Println(i, f)
+
+	const c = 20
+	i = c
+	f = c
+	fmt.Println(i, f)
+
+	var b byte = 0xff
+	var si uint8 = 0xff
+	var bi uint16 = 0xffff
+
+	fmt.Println(b, si, bi)
+
+	b++
+	si++
+	bi++
+
+	fmt.Println(b, si, bi)
 }

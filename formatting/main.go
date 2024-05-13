045660fcd87f02335cfd2c0039a1d38186d46ff7
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-13 19:31:55 +0330

    Update main.go

--- formatting/main.go
+++ formatting/main.go
@@ -28,4 +28,13 @@ func main() {
 	fmt.Printf("%4d\n", i)
 	fmt.Printf("%-4d\n", i)
 	fmt.Printf("%04d\n", i)
+
+	txt = "Hello"
+
+	fmt.Printf("%s\n", txt)
+	fmt.Printf("%q\n", txt)
+	fmt.Printf("%8s\n", txt)
+	fmt.Printf("%-8s\n", txt)
+	fmt.Printf("%x\n", txt)
+	fmt.Printf("% x\n", txt)
 }

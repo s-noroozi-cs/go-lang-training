3c2367dab4a8ace9cc5512f3d93159f4d60bdb42
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-14 21:30:41 +0330

    Update main.go

--- map/main.go
+++ map/main.go
@@ -30,9 +30,101 @@ func sampleB() {
 	fmt.Println("")
 }
 
+func sampleC() {
+	var a = make(map[string]string)
+	var b map[string]string
+
+	fmt.Println("--- Sample C ---")
+	fmt.Println(a == nil)
+	fmt.Println(b == nil)
+	fmt.Println("")
+}
+
+func sampleD() {
+	fmt.Println("--- Sample D ---")
+
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+
+	fmt.Println(a)
+
+	a["year"] = "1970" // Updating an element
+	a["color"] = "red" // Adding an element
+
+	fmt.Println(a)
+
+	fmt.Println("")
+}
+
+func sampleE() {
+	fmt.Println("--- Sample E ---")
+	var a = make(map[string]string)
+	a["brand"] = "Ford"
+	a["model"] = "Mustang"
+	a["year"] = "1964"
+
+	fmt.Println(a)
+
+	delete(a, "year")
+
+	fmt.Println(a)
+	fmt.Println("")
+}
+
+func sampleF() {
+	fmt.Println("--- Sample F ---")
+	var a = map[string]string{"brand": "Ford", "model": "Mustang", "year": "1964", "day": ""}
+
+	val1, ok1 := a["brand"] // Checking for existing key and its value
+	val2, ok2 := a["color"] // Checking for non-existing key and its value
+	val3, ok3 := a["day"]   // Checking for existing key and its value
+	_, ok4 := a["model"]    // Only checking for existing key and not its value
+
+	fmt.Println(val1, ok1)
+	fmt.Println(val2, ok2)
+	fmt.Println(val3, ok3)
+	fmt.Println(ok4)
+	fmt.Println("")
+}
+
+func sampleG() {
+	fmt.Println("--- Sample G ---")
+	a := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4}
+
+	for k, v := range a {
+		fmt.Printf("%v : %v, ", k, v)
+	}
+	fmt.Println("")
+}
+
+func sampleH() {
+	fmt.Println("--- Sample H ---")
+	a := map[string]int{"one": 1, "two": 2, "three": 3, "four": 4}
+
+	var b []string // defining the order
+	b = append(b, "one", "two", "three", "four")
+
+	for k, v := range a { // loop with no order
+		fmt.Printf("%v : %v, ", k, v)
+	}
+
+	fmt.Println()
+
+	for _, element := range b { // loop with the defined order
+		fmt.Printf("%v : %v, ", element, a[element])
+	}
+	fmt.Println("")
+}
+
 func main() {
-
 	sampleA()
-
 	sampleB()
+	sampleC()
+	sampleD()
+	sampleE()
+	sampleF()
+	sampleG()
+	sampleH()
 }

36233dc50ebba968461bc7ccd3d535c315d42fbb
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-16 13:44:35 +0330

    Update main.go

--- range/main.go
+++ range/main.go
@@ -3,5 +3,28 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("ok")
+
+	fmt.Println("---------------------------")
+
+	nums := []int{2, 3, 4}
+
+	for i, v := range nums {
+		fmt.Printf("index: %v, value: %v\n", i, v)
+	}
+
+	fmt.Println("---------------------------")
+
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	fmt.Printf("sums: %v\n", sum)
+
+	fmt.Println("---------------------------")
+
+	kv := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kv {
+		fmt.Printf("key: %v, value: %v\n", k, v)
+	}
+
 }

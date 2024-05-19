2eca26c67cf1d30519532e65ed297a4dfefc42a6
Saeid Noroozi <s.noroozi.cs@gmail.com>
2024-05-19 18:00:58 +0330

    Update main.go

--- strings-runes/main.go
+++ strings-runes/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func print(str string) {
+	fmt.Println("-------------------------------")
+
+	fmt.Printf("value: %v, len: %v\n", str, len(str))
+
+	for i := 0; i < len(str); i++ {
+		fmt.Printf("%x ", str[i])
+	}
+	fmt.Println()
+
+	fmt.Println("Rune count:", utf8.RuneCountInString(str))
+
+	for idx, runeValue := range str {
+		fmt.Printf("%#U starts at %d\n", runeValue, idx)
+	}
+
+}
 
 func main() {
-	fmt.Println("ok")
+	print("درود") // persian hello
+
+	print("Hello") // english hello
 }
